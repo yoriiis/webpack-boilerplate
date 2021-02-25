f1bd38462573461a89b188ed71ebff0b9d53d228
Yoriiis <joris.daniel@gmail.com>
2021-02-25 19:33:12 +0100

    Fix tests eslint

--- babel.config.js
+++ babel.config.js
@@ -2,13 +2,17 @@ module.exports = function (api) {
 	api.cache(true)
 
 	const presets = [['@babel/preset-env']]
-	const plugins = ['babel-plugin-dynamic-import-node', '@babel/proposal-class-properties', [
+	const plugins = [
+		'babel-plugin-dynamic-import-node',
+		'@babel/proposal-class-properties',
+		[
 			'@babel/plugin-transform-react-jsx',
-						{
-							pragma: 'createElement',
-							pragmaFrag: 'Fragment'
-						}
-					]]
+			{
+				pragma: 'createElement',
+				pragmaFrag: 'Fragment'
+			}
+		]
+	]
 
 	return {
 		presets,
